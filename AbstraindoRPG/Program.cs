fbaf945278a379f22cae86d66ccfee44ce34dd8b
manoellaalves <mao1@discente.ifpe.edu.br>
2022-02-15 11:51:52 -0300

    Adicionando solução do projeto

---
+++ AbstraindoRPG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstraindoRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
